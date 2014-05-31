7e065f288f9f7c18bc860ee3190083b1af9a80cb
Augusto Pimenta <augustoppimenta@gmail.com>
2014-05-31 14:51:20 -0300

    Inicinando projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SosDenuncia::Application.config.secret_key_base = 'b4c1bacf594bc5e69d1c536cf41cdfc109e6e7c3d509942e2192de4c872fcdb2628f0bfaf88b7f3ad5c0cb0e4aa57bb74839395704d62992be962d3c13d6cc8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SosDenuncia::Application.config.session_store :cookie_store, key: '_sos_denuncia_session'
